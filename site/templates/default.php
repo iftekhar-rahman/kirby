304bcce1566d2fe7b5ffdc7beab5e1b9c33dab97
Iftekhar Rahman <iftekharmail1@gmail.com>
2019-10-27 07:49:25 -0700

    till navigation

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,25 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $site->title() ?></title>
+</head>
+<body>
+
+    <header>
+        <a href="<?= $site->url() ?>"><?= $site->title() ?></a>
+
+        <nav class="menu">
+            <?php foreach($site->children()->listed() as $subpage) : ?>
+            <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
+            <?php endforeach ?>
+        </nav>
+    </header>
+
+    <h1><?= $page->title() ?></h1>
+    <?= $page->text() ?>
+
+</body>
+</html>
